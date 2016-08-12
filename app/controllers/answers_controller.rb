141a6000769903187592d9aa970ad4a22b01605c
ChNikS <cherednik.nickita@yandex.ru>
2016-08-12 13:27:55 -0700

    correction1 for task2

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,19 +1,19 @@
 class AnswersController < ApplicationController
-	before_action :find_question, only: [:create]
+	before_action :load_question, only: [:create]
 
 	def create
-	  @question.answers << Answer.new(answer_params)
-	  redirect_to @question
+		@question.answers << Answer.new(answer_params)
+		redirect_to @question
 	end
 
 	private
 
-	def find_question
-	  @question = Question.find(params[:question_id])
+	def load_question
+		@question = Question.find(params[:question_id])
 	end
 
 	def answer_params
-	  params.require(:answer).permit(:body)
+		params.require(:answer).permit(:body)
 	end
 
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe AnswersController, type: :controller do
 
     context 'with invalid attributes' do
       it 'does not save an answer' do
-        expect { post :create, params:{question_id: question, answer: attributes_for(:invalid_answer) } }.to_not change(question.answers, :count)
+        expect { post :create, params:{question_id: question, answer: attributes_for(:invalid_answer) } }.to_not change(Answer, :count)
       end
 
       it 'rederects to show question with answers' do
