b2a14fba7fd854a501d91281a697714ae2775015
ChNikS <cherednik.nickita@yandex.ru>
2016-08-23 02:16:31 -0700

    correction6

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,6 +1,6 @@
 module ControllerMacros
   def sign_in_user
-  	before do
+    before do
       @user = create(:user)
       @request.env['devise.mapping'] = Devise.mappings[:user]
       sign_in @user
